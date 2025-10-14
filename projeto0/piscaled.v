107dc9cc90cb1af6148c433d44a38e7c482832ad
Joaof14 <joao-pedro032011@hotmail.com>
2025-10-14 12:01:11 -0300

    "projeto 1 finalizado e inicio de projeto 2 construção em andamento de makefile do projeto 0"

--- projeto0/piscaled.v
+++ projeto0/piscaled.v
@@ -1,3 +1,23 @@
-module piscaled(input clk, c);
+module piscaled(input wire clk, 
+    output reg led);
+
+
+    //Divisor de clk
+    parameter CLK_FREQ = 27_000_000; // Frequência padrão do clock (50 MHz)
+    parameter BLINK_FREQ = 1;        // Frequência desejada do LED (1 Hz)
+
+    // Cálculo do contador (ajuste conforme necessidade)
+    localparam MAX_COUNT = CLK_FREQ / (2 * BLINK_FREQ) - 1;
+    reg [24:0] counter = 0; // Ajuste a largura conforme necessário
+
+    always @(posedge clk) begin
+        if (counter >= MAX_COUNT) begin
+            counter <= 0;
+            led <= ~led; // Inverte o estado do LED
+        end else begin
+            counter <= counter + 1;
+        end
+    end
+
 
 endmodule
--- projeto2/src/comparators.v
+++ projeto2/src/comparators.v
@@ -1,3 +1,7 @@
-module comparators();
+module comparators(
+    input [11:0] d, 
+    output m
+);
+    
 
 endmodule
