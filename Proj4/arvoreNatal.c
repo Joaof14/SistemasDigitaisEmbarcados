0d318abe43e3c07fea1568917e51c1ad25b92428
Joaof14 <joao-pedro032011@hotmail.com>
2025-12-09 09:58:53 -0300

    "primeira tentativa de código para rodar mini arvore de natal com prototipagem"

---
+++ Proj4/arvoreNatal.c
@@ -0,0 +1,46 @@
+#define F_CPU 16000000UL // Defining clock
+
+#include <avr/io.h> // Including library avr
+#include <util/delay.h> //Procurar biblioteca
+
+int main( void ){
+
+    //Setup
+
+    DDRB |= (1<<5) | (1<<4) | (1<<3) | (1<<2) | (1<<1) | (1<<0) ;
+
+    while (1) {
+        PORTB |= (1<<5); //Porta PB5 
+        _delay_ms(10000);
+        PORTB &= ~(1<<5); //Porta PB5 
+        _delay_ms(10000);
+
+        PORTB |= (1<<4); //Porta PB4
+        _delay_ms(10000);
+        PORTB &= ~(1<<4); //Porta PB4
+        _delay_ms(10000);
+
+        PORTB |= (1<<3); //Porta PB3
+        _delay_ms(10000);
+        PORTB &= ~(1<<3); //Porta PB3
+        _delay_ms(10000);
+
+        PORTB |= (1<<2); //Porta PB2
+        _delay_ms(10000);
+        PORTB &= ~(1<<2); //Porta PB2
+        _delay_ms(10000);
+
+        PORTB |= (1<<1); //Porta PB1 
+        _delay_ms(10000);
+        PORTB &= ~(1<<1); //Porta PB1
+        _delay_ms(10000);
+
+        PORTB |= (1<<0); //Porta PB0
+        _delay_ms(10000);
+        PORTB &= ~(1<<0); //Porta PB0
+        _delay_ms(10000);
+    }
+
+  return 0;
+}
+
